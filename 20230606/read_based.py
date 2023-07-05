336c9f95113b7172b485485c290e9a3d2679236d
Niccolo Tesi <tesinicco@gmail.com>
2023-07-05 12:59:40 +0200

    Update read_based.py

--- 20230606/read_based.py
+++ 20230606/read_based.py
@@ -6,8 +6,35 @@
 import sys
 
 # Functions
+# Read bed file
+def readBed(bed_dir):
+    bed = {}
+    count_reg = 0
+    with open(bed_dir) as finp:
+        for line in finp:
+            if line.startswith('#'):
+                pass
+            else:
+                line = line.rstrip().split()
+                if len(line) >= 3:
+                    chrom, start, end = line[0:3]
+                    region_id = chrom + ':' + start + '-' + end
+                    count_reg += 1
+                    if 'chr' not in chrom:
+                        chrom = 'chr' + str(chrom)
+                    if chrom in bed.keys():
+                        bed[chrom].append([start, end, region_id])
+                    else:
+                        bed[chrom] = [[start, end, region_id]]
+    print('**** Found %s regions in %s chromosomes' %(count_reg, len(bed)))
+    return bed
 
 # Main
 # Read arguments
-inBam, bed, outDir, ref, window, cpu, phasingData, mappingSNP, HaploDev, minimumSupport, minimumCoverage = sys.argv[1::]
-print(inBam, bed, outDir)
+inBam, bed_dir, outDir, ref, window, cpu, phasingData, mappingSNP, HaploDev, minimumSupport, minimumCoverage = sys.argv[1::]
+
+# Start analysis
+print('** Analysis started')
+# Read bed file
+bed = readBed(bed_dir)
+
