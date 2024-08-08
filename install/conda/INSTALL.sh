cfe5db9496f50b9a3bab8d757f1f83383d98d409
Niccolo Tesi <holstegelab-ntesi@ui-02.spider.surfsara.nl>
2024-08-08 15:16:20 +0200

    install

--- install/conda/INSTALL.sh
+++ install/conda/INSTALL.sh
@@ -9,34 +9,21 @@ conda env create -f treat.yml
 echo "**** TREAT successfully installed"
 
 chmod +x $PWD/TREAT.py
-conda activate treat
+conda activate newtreat
 
-echo "**** Now installing HTS lib"
+echo "**** Now installing Otter"
 
-wget https://github.com/samtools/htslib/releases/download/1.19.1/htslib-1.19.1.tar.bz2 .
-tar -xvf htslib-1.19.1.tar.bz2
-cd htslib-1.19.1
-./configure --prefix=$PWD
-make
-make install
-
-echo "**** Installing Otter"
-cd ..
-export CPATH=$CPATH:$PWD/htslib-1.19.1
-export LIBRARY_PATH=$LIBRARY_PATH:$PWD/htslib-1.19.1
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/htslib-1.19.1
-echo export CPATH=$CPATH:$PWD/htslib-1.19.1 > $PWD/activate_env.sh
-echo export LIBRARY_PATH=$LIBRARY_PATH:$PWD/htslib-1.19.1 >> $PWD/activate_env.sh
-echo export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/htslib-1.19.1 >> $PWD/activate_env.sh
-git clone https://github.com/holstegelab/otter.git && cd otter
+cd ../../bin/
+git clone --branch development --recursive https://github.com/holstegelab/otter.git
+cd otter/include/WFA2-lib
+make clean setup lib_wfa
+cd ../../
 mkdir build
 make packages
 make
+cd ..
 
-echo "** Exporting packages to main path"
-cd ../../
-
-echo "export PATH=$PWD/bin/otter/build/:$PATH" >> $PWD/bin/activate_env.sh
-echo "export PATH+=:$PWD/bin/:$PATH" >> $PWD/bin/activate_env.sh
+echo "export PATH=$PWD/otter/build/:$PATH" >> $PWD/bin/activate_env.sh
+echo "export PATH+=:$PWD/:$PATH" >> $PWD/bin/activate_env.sh
 chmod +x $PWD/bin/activate_env.sh
 mv $PWD/bin/activate_env.sh $CONDA_PREFIX/etc/conda/activate.d/
--- install/docker/docker_run.sh
+++ install/docker/docker_run.sh
@@ -1,66 +1,101 @@
-# Example commands to run TREAT/Otter with Docker
+################################################
+################################################
+# This script contains example scripts to run TREAT from the docker environment.
+# You can comment/uncomment lines depending on the script of interest.
+################################################
+################################################
 
-# To build the Docker image, use the following command:
-docker build --network host -t treat .
-
-# When the image is built, use the following script to run TREAT/Otter docker image
-
-# Set up variables and data to use
+# Set up variables for running
+# Assembly-based analysis
 INPUT_BAM=/root/TREAT/data/test_data/HPC_example/
 INPUT_BED=/root/TREAT/data/test_data/example.bed
 REFERENCE=/root/TREAT/data/GRCh38_full_analysis_set_plus_decoy_hla.fa
-OUTPUT=/root/TREAT/data/outfolder
-VCF=/root/TREAT/data/outfolder/sample.vcf.gz
-OUTPUT_ANALYSIS=/root/TREAT/data/outfolder/outlierAnalysis
-OUTPUT_ANALYSIS_CC=/root/TREAT/data/outfolder/CaseControlAnalysis
+OUTPUT_ASM=/root/TREAT/data/TREAT_assembly
+
+# Read-based analysis
+OUTPUT_READS=/root/TREAT/data/TREAT_reads
+
+# Outlier analysis
+VCF_ASM=/root/TREAT/data/TREAT_assembly/sample.vcf.gz
+VCF_READS=/root/TREAT/data/TREAT_reads/sample.vcf.gz
+OUTPUT_ANALYSIS_ASM=/root/TREAT/data/outlierAnalysis_Assembly
+OUTPUT_ANALYSIS_READS=/root/TREAT/data/outlierAnalysis_Reads
+KNOWN_BOUNDARIES=/root/TREAT/data/test_data/HPC_example/known_boundaries.txt
+
+# Case-control analysis
+OUTPUT_ANALYSIS_CC_ASM=/root/TREAT/data/CaseControlAnalysis_Assembly
+OUTPUT_ANALYSIS_CC_READS=/root/TREAT/data/CaseControlAnalysis_Reads
 CC_LABELS=/root/TREAT/data/test_data/HPC_example/case_control_labels.txt
+
+# Plot
+OUTPUT_PLOT_ASM=/root/TREAT/data/Plot_Assembly
+OUTPUT_PLOT_READS=/root/TREAT/data/Plot_Reads
+
+###############################################
+###############################################
+# Commands for running
 
 # TREAT assembly based analysis command
 docker run -it --rm \
-       -v ${INPUT_BAM}:/run_input.bam \
-       -v ${INPUT_BAM}.bai:/run_input.bam.bai \
-       -v ${INPUT_BED}:/run_input.bed \
-       -v ${REFERENCE}:/run_input_ref.fa \
-       -v ${REFERENCE}.fai:/run_input_ref.fa.fai \
-       -v ${OUTPUT}:/run_outfolder \
-       treat \
-       assembly \
-       -i /run_input.bam \
-       -b /run_input.bed \
-       -r /run_input_ref.fa \
-       -o /run_outfolder
+        -v ${INPUT_BAM}:/run_input.bam \
+        -v ${INPUT_BAM}.bai:/run_input.bam.bai \
+        -v ${INPUT_BED}:/run_input.bed \
+        -v ${REFERENCE}:/run_input_ref.fa \
+        -v ${REFERENCE}.fai:/run_input_ref.fa.fai \
+        -v ${OUTPUT_ASM}:/run_outfolder \
+        treat \
+        assembly \
+        -i /run_input.bam \
+        -b /run_input.bed \
+        -r /run_input_ref.fa \
+        -o /run_outfolder
 
 # TREAT read based analysis command
 docker run -it --rm \
-       -v ${INPUT_BAM}:/run_input.bam \
-       -v ${INPUT_BAM}.bai:/run_input.bam.bai \
-       -v ${INPUT_BED}:/run_input.bed \
-       -v ${REFERENCE}:/run_input_ref.fa \
-       -v ${REFERENCE}.fai:/run_input_ref.fa.fai \
-       -v ${OUTPUT}:/run_outfolder \
-       treat \
-       reads \
-       -i /run_input.bam \
-       -b /run_input.bed \
-       -r /run_input_ref.fa \
-       -o /run_outfolder
+        -v ${INPUT_BAM}:/run_input.bam \
+        -v ${INPUT_BAM}.bai:/run_input.bam.bai \
+        -v ${INPUT_BED}:/run_input.bed \
+        -v ${REFERENCE}:/run_input_ref.fa \
+        -v ${REFERENCE}.fai:/run_input_ref.fa.fai \
+        -v ${OUTPUT_READS}:/run_outfolder \
+        treat \
+        reads \
+        -i /run_input.bam \
+        -b /run_input.bed \
+        -r /run_input_ref.fa \
+        -o /run_outfolder
 
-# TREAT outlier analysis command
-docker run -it --rm \
-       -v ${INPUT_BED}:/run_input.bed \
-       -v ${OUTPUT_ANALYSIS}:/run_outfolder \
-       -v ${VCF}:/run_input.vcf.gz \
-       treat \
-       analysis \
-       -a outlier \
-       -v /run_input.vcf.gz \
-       -o /run_outfolder
-
-# TREAT case-control analysis command
+# TREAT outlier analysis command based on Assembly output
 docker run -it --rm \
         -v ${INPUT_BED}:/run_input.bed \
-        -v ${OUTPUT_ANALYSIS_CC}:/run_outfolder_cc \
-        -v ${VCF}:/run_input.vcf.gz \
+        -v ${OUTPUT_ANALYSIS_ASM}:/run_outfolder \
+        -v ${VCF_ASM}:/run_input.vcf.gz \
+        -v ${KNOWN_BOUNDARIES}:/run_known_boundaries.txt \
+        treat \
+        analysis \
+        -a outlier \
+        -v /run_input.vcf.gz \
+        -o /run_outfolder \
+        -k /run_known_boundaries.txt
+
+# TREAT outlier analysis command based on Reads output
+docker run -it --rm \
+        -v ${INPUT_BED}:/run_input.bed \
+        -v ${OUTPUT_ANALYSIS_READS}:/run_outfolder \
+        -v ${VCF_READS}:/run_input.vcf.gz \
+        -v ${KNOWN_BOUNDARIES}:/run_known_boundaries.txt \
+        treat \
+        analysis \
+        -a outlier \
+        -v /run_input.vcf.gz \
+        -o /run_outfolder \
+        -k /run_known_boundaries.txt
+
+# TREAT case-control analysis command based on Assembly output
+docker run -it --rm \
+        -v ${INPUT_BED}:/run_input.bed \
+        -v ${OUTPUT_ANALYSIS_CC_ASM}:/run_outfolder_cc \
+        -v ${VCF_ASM}:/run_input.vcf.gz \
         -v ${CC_LABELS}:/run_input_labels.txt \
         treat \
         analysis \
@@ -68,3 +103,36 @@ docker run -it --rm \
         -v /run_input.vcf.gz \
         -l /run_input_labels.txt \
         -o /run_outfolder_cc
+
+# TREAT case-control analysis command based on Reads output
+docker run -it --rm \
+        -v ${INPUT_BED}:/run_input.bed \
+        -v ${OUTPUT_ANALYSIS_CC_READS}:/run_outfolder_cc \
+        -v ${VCF_READS}:/run_input.vcf.gz \
+        -v ${CC_LABELS}:/run_input_labels.txt \
+        treat \
+        analysis \
+        -a case-control \
+        -v /run_input.vcf.gz \
+        -l /run_input_labels.txt \
+        -o /run_outfolder_cc
+
+# TREAT plot command based on Assembly output
+docker run -it --rm \
+        -v ${OUTPUT_PLOT_ASM}:/run_outfolder_plot \
+        -v ${VCF_ASM}:/run_input.vcf.gz \
+        treat \
+        plot \
+        -v /run_input.vcf.gz \
+        -r all \
+        -o /run_outfolder_plot
+
+# TREAT plot command based on Reads output
+docker run -it --rm \
+        -v ${OUTPUT_PLOT_READS}:/run_outfolder_plot \
+        -v ${VCF_READS}:/run_input.vcf.gz \
+        treat \
+        plot \
+        -v /run_input.vcf.gz \
+        -r all \
+        -o /run_outfolder_plot
