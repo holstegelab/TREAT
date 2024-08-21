fb68a7f4e6201330da64dcb9de7a37f73ebf7200
TesiNicco <holstegelab-ntesi@ui-02.spider.surfsara.nl>
2024-08-21 11:58:22 +0200

    merge

--- bin/merge_vcf.py
+++ bin/merge_vcf.py
@@ -75,60 +75,50 @@ def skip_comment_lines(file):
     return pd.DataFrame(lines[1::], columns = lines[0])
 
 # Combine 2 dataframes resembling the VCF
-def combine_2_df(df1, df2):
-    common_df = pd.DataFrame()
-    # iterates over the rows of dataframe 2 (not the merged)
-    for index, row in df2.iterrows():
-        # take regions
-        region2 = row['ID']
-        # create dictionary with alleles mapping
-        al_map = {}
-        all_alt = []
-        # check if region is in merged df1
-        if region2 in list(df1['ID']):
-            temp_df = df1[df1['ID'] == region2].copy()
-            # then extract the alleles from both
-            alt2 = row['ALT'].split(',')
-            all_alt = [x.split(',') for x in temp_df['ALT']][0]
-            # check if alleles are the same, otherwise add them to the merged df
-            for al in alt2:
-                if al in all_alt:
-                    al_index = all_alt.index(al) + 1
-                    al_map[alt2.index(al) + 1] = al_index
+def combine_opt(df1, df2):
+    # first merge df1 (entire) with df2 (sample-specific)
+    df2_subset = df2.iloc[:, [2, 4] + list(range(9, df2.shape[1]))].copy()
+    df2_subset.rename(columns={df2_subset.columns[1]: 'ALT_2'}, inplace=True)
+    merged_df = pd.merge(df1, df2_subset, on='ID', how='outer')
+    # now that it's merged, i need to fix the alleles
+    for i in range(merged_df.shape[0]):
+        percentage = round(i/merged_df.shape[0]*100, 2)
+        print(f'{percentage:.2f}%', end='\r')
+        # get alleles from df1 and df2
+        df1_al = merged_df.iat[i, merged_df.columns.get_loc('ALT')]
+        df2_al = merged_df.iat[i, merged_df.columns.get_loc('ALT_2')]
+        # check nan
+        if not pd.isna(df1_al) and not pd.isna(df2_al):
+            df1_al = df1_al.split(',')
+            df2_al = df2_al.split(',')
+            # define an allele mapping dictionary
+            al_map = {0 : 0}
+            # iterate through df2 alleles
+            for al in df2_al:
+                if al in df1_al:
+                    # if this is a known allele, get the index
+                    al_index = df1_al.index(al)
+                    al_map[df2_al.index(al) + 1] = al_index + 1
                 else:
-                    all_alt.append(al)
-                    al_index = len(all_alt)
-                    al_map[alt2.index(al) + 1] = al_index
-            # after checking the alleles, we need to reformat the samples
-            temp_df['ALT'] = ','.join(all_alt)
-            for col in df2.iloc[:, 9:]:
-                tmp_value = row[col]
-                # update the genotype based on the mapping dictionary
-                tmp_mod = np.array([replace_field(arr = tmp_value, replace_dict = al_map)])
-                temp_df[col] = tmp_mod
-            # finally add to the common df
-            common_df = pd.concat([common_df, temp_df])
-        else:
-            temp_df = pd.DataFrame([row.values[0:9]], columns = ['#CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT'])
-            # add all samples from df1
-            for col in df1.iloc[:, 9:]:
-                temp_df[col] = 'NA;NA;NA;NA;NA;NA;NA'
-            # then add all samples from df2
-            for col in range(9, len(row)):
-                temp_df[df2.columns[col]] = row[col]
-            # finally add to the common df
-            common_df = pd.concat([common_df, temp_df])
-    # then we need to add the rows of dataframe 1 not in 2
-    for index, row in df1.iterrows():
-        region1 = row['ID']
-        if region1 not in list(df2['ID']):
-            temp_df = pd.DataFrame([row.values], columns = df1.columns)
-            # then add all samples from df2
-            for col in df2.iloc[:, 9:]:
-                temp_df[col] = 'NA;NA;NA;NA;NA;NA;NA'
-            # finally add to the common df
-            common_df = pd.concat([common_df, temp_df])
-    return common_df
+                    # otherwise, save the mapping and update the allele list
+                    df1_al.append(al)
+                    al_index = df1_al.index(al)
+                    al_map[df2_al.index(al) + 1] = al_index + 1
+            # update alleles
+            merged_df.iat[i, 4] = ','.join(df1_al)
+            # then need to update the genotypes
+            for col in range(df1.shape[1] + 1, merged_df.shape[1]):
+                merged_df.iat[i, col] = replace_field(arr = merged_df.iat[i, col], replace_dict = al_map)
+        elif pd.isna(df1_al) and not pd.isna(df2_al):
+            # here all samples from df1 should be NA;NA;NA;NA;NA;NA;NA
+            for col in range(9, df1.shape[1]):
+                merged_df.iat[i, col] = 'NA;NA;NA;NA;NA;NA;NA'
+        elif not pd.isna(df1_al) and pd.isna(df2_al):
+            # here all samples from df2 should be NA;NA;NA;NA;NA;NA;NA
+            for col in range(df1.shape[1] + 1, merged_df.shape[1]):
+                merged_df.iat[i, col] = 'NA;NA;NA;NA;NA;NA;NA'
+    merged_df = merged_df.drop('ALT_2', axis = 1)
+    return merged_df
 
 # Function to replace array entry based on a dictionary
 def replace_field(arr, replace_dict):
@@ -163,7 +153,7 @@ for f in vcf[1::]:
     print('** combining VCF %s/%s' %(vcf.index(f) + 1, len(vcf)))
     tmp = skip_comment_lines(f)
     # combine the 2 dfs
-    combined_df = combine_2_df(df1 = combined_df, df2 = tmp)
+    combined_df = combine_opt(df1 = combined_df, df2 = tmp)
 
 # 3. write output file
 output_fname = '%s/%s' %(outDir, outName)
